a67e973937aaf8b2ef6a4ebd9c551825995705a7
enriquemadge <69826261+enriquemadge@users.noreply.github.com>
2020-08-19 18:55:02 -0500

    4ra subida

--- index.py
+++ index.py
@@ -3,25 +3,23 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Creating simple Routes 
-#@app.route('/test')
-#def test():
- ##   return "Home Page"
+@app.route('/test')
+def test():
+    return "Home Page"
 
-#@app.route('/test/about/')
-#def about_test():
-    #return "About Page"
+@app.route('/test/about/')
+def about_test():
+    return "About Page"
 
 # Routes to Render Something
 @app.route('/')
 def home():
     return render_template("home.html")
 
-# @app.route('/about', strict_slashes=False)
-@app.route('/about')
+@app.route('/about', strict_slashes=False)
 def about():
     return render_template("about.html")
 
-# Make sure this we are executing this file   
+# Make sure this we are executing this file
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run()
+    app.run(debug=True)
